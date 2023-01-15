a231208d83e6dd11eeb3c75727e1840785c13a1f
garden-jun <jeongwonjun0718@gmail.com>
2023-01-15 17:29:54 +0900

    register Blueprint (main_views.bp)

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ def create_app():
     # app.py 라는 모듈이 실행되므로 __name__ 변수에는 pybo라는 문자열이 담긴다.
     app = Flask(__name__)
 
+    """
+    blueprint 생성 이전 사용한 라우팅 함수
     # @app.route는 URL과 플라스크 코드를 매핑하는 데코레이터
     # / URL이 요청되면 플라스크는 hello_pybo 함수를 실행시킨다.
     @app.route('/')
     def hello_pybo():
         return 'Hello, Pybo!'
+    """
+
+    from .views import main_views
+    app.register_blueprint(main_views.bp)   # main_views.bp 파일에 있는 블루프린트 객체 bp를 등록
 
     return app
