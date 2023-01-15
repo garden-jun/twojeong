55b97523d78ce345c2a9daa82f4263865e4622fe
garden-jun <jeongwonjun0718@gmail.com>
2023-01-16 00:17:47 +0900

    ORM 적용

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
+import config
+
+# 전역변수로 db, migrate 객체를 생성
+db = SQLAlchemy()
+migrate = Migrate()
 
 # create_app은 플라스크 내부에서 정의된 함수명으로 다른 이름을 사용하면 정상적으로 동작하지 않는다.
 def create_app():
     # app.py 라는 모듈이 실행되므로 __name__ 변수에는 pybo라는 문자열이 담긴다.
     app = Flask(__name__)
+    app.config.from_object(config)  # config.py에 작성된 항목을 읽음
+
+    # ORM
+    db.init_app(app)    # db를 app에 등록
+    migrate.init_app(app, db)   # migrate를 app에 등록
 
     """
     blueprint 생성 이전 사용한 라우팅 함수
@@ -15,6 +27,7 @@ def create_app():
         return 'Hello, Pybo!'
     """
 
+    # 블루프린트
     from .views import main_views
     app.register_blueprint(main_views.bp)   # main_views.bp 파일에 있는 블루프린트 객체 bp를 등록
 
